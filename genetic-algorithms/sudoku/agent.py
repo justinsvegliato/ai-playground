2cb5216b95b8663019aa74e890b9f45ae969c3c4
Justin Svegliato <justin.svegliato1@marist.edu>
2016-04-22 21:37:57 -0400

    Implemented a basic sudoku solver using genetic algorithms (but it converges very slowly)

---
+++ genetic-algorithms/sudoku/agent.py
@@ -0,0 +1,111 @@
+import sudoku
+import random
+
+POPULATION_SIZE = 30
+GENERATIONS = 100000
+ELITE_MEMBER_COUNT = 5
+MUTATION_LIMIT = 10
+
+def get_fitness_score(puzzle):
+    fitness_score = 0
+
+    for i in range(sudoku.PUZZLE_SIZE):
+        if sudoku.is_column_valid(puzzle, i):
+            fitness_score += 1
+
+        if sudoku.is_row_valid(puzzle, i):
+            fitness_score += 1
+
+        if sudoku.is_square_valid(puzzle, i):
+            fitness_score += 1
+
+    return fitness_score
+
+def get_initial_population(initial_puzzle, capacity):
+    population = []
+
+    for puzzle_id in range(capacity):
+        puzzle = initial_puzzle
+
+        for row_id, column_id in sudoku.get_empty_cells(initial_puzzle):
+            value = get_cell_value(puzzle, row_id, column_id)
+            puzzle = sudoku.set_cell(puzzle, row_id, column_id, value)
+
+        population.append(puzzle)
+
+    return population
+
+def get_cell_value(puzzle, row_id, column_id):
+    if random.random() < 0.2:
+        return random.choice(sudoku.DIGITS)
+
+    cells = sudoku.get_column(puzzle, column_id) if random.random() > 0.5 else sudoku.get_row(puzzle, row_id)
+    values = sudoku.get_remaining_values(cells)
+
+    return random.choice(values) if len(values) > 0 else random.choice(sudoku.DIGITS)
+
+def get_enriched_population(population):
+    enriched_population = []
+
+    for puzzle_id in range(len(population)):
+        puzzle = population[puzzle_id]
+        fitness = get_fitness_score(puzzle)
+        enriched_population.append([puzzle, fitness])
+
+    return enriched_population
+
+def get_elite_members(population, capacity):
+    enriched_population = get_enriched_population(population)
+    sorted_population = sorted(enriched_population, key=lambda x: x[1], reverse=True)
+    raw_population = [puzzle[0] for puzzle in sorted_population]
+    return raw_population[:capacity]
+
+def get_mutated_puzzle(initial_puzzle, puzzle):
+    empty_cells = sudoku.get_empty_cells(initial_puzzle)
+
+    mutations = random.choice(range(MUTATION_LIMIT)) + 1
+
+    mutated_puzzle = puzzle
+    for mutation in range(mutations):
+        empty_cell = random.choice(empty_cells)
+        value = get_cell_value(puzzle, empty_cell[0], empty_cell[1])
+        mutated_puzzle = sudoku.set_cell(mutated_puzzle, empty_cell[0], empty_cell[1], value)
+
+    return mutated_puzzle
+
+def get_average_fitness_score(population):
+    fitness_score_sum = 0
+
+    for i in range(len(population)):
+        fitness_score_sum += get_fitness_score(population[i])
+
+    return fitness_score_sum / len(population)
+
+def get_highest_fitness_score(population):
+    enriched_population = get_enriched_population(population)
+    return max(enriched_population, key=lambda x:x[1])[1]
+
+def main():
+    initial_puzzle = sudoku.load_puzzle("easy_puzzle_2.csv")
+
+    population = get_initial_population(initial_puzzle, POPULATION_SIZE)
+
+    for generation in range(GENERATIONS):
+        if sudoku.is_puzzle_valid(population[0]):
+            break
+
+        new_population = get_elite_members(population, ELITE_MEMBER_COUNT)
+
+        for i in range(POPULATION_SIZE - ELITE_MEMBER_COUNT):
+            puzzle = random.choice(population)
+            mutated_puzzle = get_mutated_puzzle(initial_puzzle, puzzle)
+            new_population.append(mutated_puzzle)
+
+        population = new_population
+
+        print "Generation %d's Fitness Score: %d" % (generation, get_highest_fitness_score(population))
+
+    print "Best Solution:"
+    print population[0]
+
+main()
--- genetic-algorithms/sudoku/sudoku.py
+++ genetic-algorithms/sudoku/sudoku.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 PUZZLE_SIZE = 9
+SQUARE_SIZE = 3
 
 DIGITS = np.array(range(1, PUZZLE_SIZE + 1))
 
@@ -8,13 +9,20 @@ def get_empty_puzzle():
     return np.zeros((PUZZLE_SIZE, PUZZLE_SIZE))
 
 def set_cell(puzzle, row_id, column_id, value):
-    puzzle[row_id, column_id] = int(value)
+    new_puzzle = np.copy(puzzle)
+    new_puzzle[row_id, column_id] = int(value)
+    return new_puzzle
 
 def get_column(puzzle, column_id):
     return puzzle[:, column_id]
 
 def get_row(puzzle, row_id):
     return puzzle[row_id, :]
+
+def get_square(puzzle, square_id):
+    start_row_id = (square_id // SQUARE_SIZE) * SQUARE_SIZE
+    start_column_id = (square_id % SQUARE_SIZE) * SQUARE_SIZE
+    return puzzle[start_row_id:start_row_id + SQUARE_SIZE, start_column_id:start_column_id + SQUARE_SIZE]
 
 def is_row_valid(puzzle, row_id):
     return set(DIGITS) == set(get_row(puzzle, row_id))
@@ -23,19 +31,20 @@ def is_column_valid(puzzle, column_id):
     return set(DIGITS) == set(get_column(puzzle, column_id))
 
 def is_square_valid(puzzle, square_id):
-    pass
+    return set(DIGITS) == set(get_square(puzzle, square_id).flatten())
 
-p = get_empty_puzzle()
-set_cell(p, 0, 0, 9)
-set_cell(p, 1, 0, 8)
-set_cell(p, 2, 0, 7)
-set_cell(p, 3, 0, 6)
-set_cell(p, 4, 0, 5)
-set_cell(p, 5, 0, 4)
-set_cell(p, 6, 0, 3)
-set_cell(p, 7, 0, 2)
-set_cell(p, 8, 0, 1)
+def is_puzzle_valid(puzzle):
+    for i in range(PUZZLE_SIZE):
+        if not (is_row_valid(puzzle, i) and is_column_valid(puzzle, i) and is_square_valid(puzzle, i)):
+            return False
+    return True
 
-print is_row_valid(p, 0)
-print is_column_valid(p, 0)
-print p
+def load_puzzle(file):
+    return np.loadtxt(open(file, "rb"), delimiter=",")
+
+def get_empty_cells(puzzle):
+    empty_cells = np.nonzero(puzzle == 0)
+    return zip(empty_cells[0], empty_cells[1])
+
+def get_remaining_values(cells):
+    return np.setdiff1d(DIGITS, cells)
